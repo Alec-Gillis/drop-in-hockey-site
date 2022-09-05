e4556698048dfe331761cbcdb60e9ab9207fd206
Alec Gillis <gillismail1213@gmail.com>
2022-09-05 16:22:04 -0400

    Initial Player model & migration

--- roster/models.py
+++ roster/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Player(models.Model):
+	name = models.CharField(max_length=35)
+	is_checked_in = models.BooleanField(default=False)
+	is_goalie = models.BooleanField(default=False)
+
+	def __str__(self):
+		return self.name
