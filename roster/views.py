737010af94b027394707f0da5d797a7cd1d1d4bc
Alec Gillis <gillismail1213@gmail.com>
2022-09-07 17:59:23 -0400

    Show checkmarks on refresh if player is checked in

--- roster/views.py
+++ roster/views.py
@@ -1,3 +1,4 @@
+from lzma import is_check_supported
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,10 +8,12 @@ from .models import Player
 
 
 def index(request):
+    skaters_count = Player.objects.filter(is_checked_in=True)
     all_skaters = Player.objects.filter(is_goalie=False)
     all_goalies = Player.objects.filter(is_goalie=True)
 
     context = {
+        'skaters_count': skaters_count,
         'all_skaters': all_skaters,
         'all_goalies': all_goalies,
     }
