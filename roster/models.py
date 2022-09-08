ce2722e2a0eec774afd8bd395b4779fc18897123
Alec Gillis <gillismail1213@gmail.com>
2022-09-07 23:35:54 -0400

    Add player substitutes

--- roster/models.py
+++ roster/models.py
@@ -5,6 +5,7 @@ class Player(models.Model):
 	name = models.CharField(max_length=35)
 	is_checked_in = models.BooleanField(default=False)
 	is_goalie = models.BooleanField(default=False)
+	is_substitute = models.BooleanField(default=False)
 
 	def __str__(self):
 		return self.name
--- roster/views.py
+++ roster/views.py
@@ -9,12 +9,14 @@ from .models import Player
 
 def index(request):
     skaters_count = Player.objects.filter(is_checked_in=True)
-    all_skaters = Player.objects.filter(is_goalie=False)
+    all_skaters = Player.objects.filter(is_goalie=False, is_substitute=False)
     all_goalies = Player.objects.filter(is_goalie=True)
+    all_subs = Player.objects.filter(is_substitute=True)
     context = {
         'skaters_count': skaters_count,
         'all_skaters': all_skaters,
         'all_goalies': all_goalies,
+        'all_subs': all_subs,
     }
     print("Request Method: " + request.method)
     if request.method == "POST":
