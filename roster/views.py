9496d1ead86ca713e47ffef279c33310e3485475
Alec Gillis <gillismail1213@gmail.com>
2022-09-08 17:21:46 -0400

    Order results; fix-up waitlist logic

--- roster/views.py
+++ roster/views.py
@@ -6,8 +6,8 @@ from .models import Player, HeaderText
 # Create your views here.
 def index(request):
     all_skaters = Player.objects.filter(is_goalie=False, is_substitute=False).order_by('name')
-    all_goalies = Player.objects.filter(is_goalie=True)
-    all_subs = Player.objects.filter(is_substitute=True)
+    all_goalies = Player.objects.filter(is_goalie=True).order_by('name')
+    all_subs = Player.objects.filter(is_substitute=True).order_by('name')
     # when the user hits submit, this will update the db
     if request.method == "POST":
         everyone = Player.objects.all()
@@ -22,9 +22,21 @@ def index(request):
                 player.update(is_checked_in=True, time_checked_in=str(timezone.now()))
 
     waiters = {}
-    wait_list = Player.objects.filter(is_goalie=False, is_checked_in=True).order_by('time_checked_in')
-    if len(wait_list) > 22:
-        waiters = wait_list[22:]
+    checked_in_waiters = Player.objects.filter(is_goalie=False, is_substitute=True, is_checked_in=True)
+    all_checked_in = len(checked_in_waiters) + len(all_skaters.filter(is_checked_in=True))
+    wait_list = Player.objects.filter(is_goalie=False, is_substitute=True, is_checked_in=True).order_by('time_checked_in')
+    while len(wait_list) and all_checked_in < 22:
+        wait_list.first().update(is_checked_in=True) 
+        wait_list = Player.objects.filter(is_goalie=False, is_substitute=True, is_checked_in=True).order_by('time_checked_in')
+    waiters = wait_list.order_by('name')
+
+    # Determine how many people need to sign out before next person is signed in
+    signout_amount = 0
+    if len(all_skaters.filter(is_checked_in=True)) >= 22:
+        signout_amount = len(all_skaters.filter(is_checked_in=True)) - 21
+    else:
+        signout_amount = 1
+
     headertext = HeaderText.objects.first()
     context = {
         'all_skaters': all_skaters,
@@ -32,6 +44,7 @@ def index(request):
         'all_subs': all_subs,
         'headertext': headertext,
         'waiters': waiters,
+        'signout_amount': signout_amount,
     }
 
     return render(request, 'roster/index.html', context)
