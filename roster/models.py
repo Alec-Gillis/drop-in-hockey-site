7be3a427e32fff10cf9c2f85965278a8528a8f5a
Alec Gillis <gillismail1213@gmail.com>
2022-09-08 00:22:45 -0400

    Add check-in time logic for players

--- roster/models.py
+++ roster/models.py
@@ -4,11 +4,12 @@ from django.db import models
 class Player(models.Model):
 	name = models.CharField(max_length=35)
 	is_checked_in = models.BooleanField(default=False)
+	time_checked_in = models.DateTimeField()
 	is_goalie = models.BooleanField(default=False)
 	is_substitute = models.BooleanField(default=False)
 
 	def __str__(self):
-		return self.name
+		return "{0} {1}".format(self.name, self.time_checked_in)
 
 class HeaderText(models.Model):
 	"""
--- roster/views.py
+++ roster/views.py
@@ -1,30 +1,31 @@
 from django.shortcuts import render
+from django.utils import timezone 
 
 from .models import Player, HeaderText
 
 # Create your views here.
 def index(request):
-    skaters_count = Player.objects.filter(is_checked_in=True)
     all_skaters = Player.objects.filter(is_goalie=False, is_substitute=False)
     all_goalies = Player.objects.filter(is_goalie=True)
     all_subs = Player.objects.filter(is_substitute=True)
     headertext = HeaderText.objects.first()
     context = {
-        'skaters_count': skaters_count,
         'all_skaters': all_skaters,
         'all_goalies': all_goalies,
         'all_subs': all_subs,
         'headertext': headertext,
     }
-    print("Request Method: " + request.method)
     if request.method == "POST":
-        print("STUFF: ", request.POST)
-        all_skaters.update(is_checked_in=False)
+        everyone = Player.objects.all()
         id_list = request.POST.getlist('player')
-        # Uncheck all players
-        print(id_list)
+        for x in everyone:
+            if str(x.id) not in id_list:
+                Player.objects.filter(pk=int(x.id)).update(is_checked_in=False)
         # Update the DB
         for x in id_list:
-            Player.objects.filter(pk=int(x)).update(is_checked_in=True)
+            player = Player.objects.filter(pk=int(x))
+            if not player[0].is_checked_in:
+                player.update(is_checked_in=True, time_checked_in=str(timezone.now()))
+
     return render(request, 'roster/index.html', context)
 
