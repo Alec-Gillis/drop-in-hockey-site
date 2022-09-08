9f347a98ea2a6aeff763d28270b25b21894f370e
Alec Gillis <gillismail1213@gmail.com>
2022-09-08 11:53:30 -0400

    Add more settings

--- drop_in_hockey_site/settings.py
+++ drop_in_hockey_site/settings.py
@@ -18,6 +18,7 @@ from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+IS_HEROKU = "DYNO" in os.environ
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
@@ -29,9 +30,14 @@ if 'SECRET_KEY' in os.environ:
     SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False 
+if not IS_HEROKU:
+    DEBUG = False 
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1', 'hockey-night-in-hartland.herokuapp.com']
+# Generally avoid wildcards(*). However since Heroku router provides hostname validation it is ok
+if IS_HEROKU:
+    ALLOWED_HOSTS = ['*']
+else:
+    ALLOWED_HOSTS= []
 
 
 # Application definition
