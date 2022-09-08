436b35dff7405802afaa855cf087d14439febb72
Alec Gillis <gillismail1213@gmail.com>
2022-09-08 16:35:29 -0400

    oopsie

--- drop_in_hockey_site/settings.py
+++ drop_in_hockey_site/settings.py
@@ -102,19 +102,26 @@ WSGI_APPLICATION = 'drop_in_hockey_site.wsgi.application'
 
 MAX_CONN_AGE = 600
 
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST'),
-        'PORT':  config('DB_PORT'),
+if not IS_HEROKU:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT':  config('DB_PORT'),
+        }
     }
-}
 
 # Change this to true if you want to use local Sqlite
+if True:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable
@@ -160,7 +167,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/images/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'statics')
 ]
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
